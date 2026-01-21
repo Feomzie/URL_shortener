a3e7dec6df82a5d24b49f6a76356659634c9bf38
Feomzie <joyogboh@gmail.com>
2026-01-21 21:27:53 +0100

    feat: codebase and database setup

---
+++ app/core/models/short_urls.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, DateTime, func 
+from app.db.database import Base
+
+
+class ShortURL(Base):
+    __table_name__ = "short_urls"
+
+    id = Column(Integer, primary_key=True)
+    original_url = Column(String, nullable=False)
+    short_url = Column(String, unique=True, index=True, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())

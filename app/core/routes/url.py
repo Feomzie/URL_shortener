b12f339d4c3fb9570675699f4e37021391cdb2ff
Feomzie <joyogboh@gmail.com>
2026-01-27 21:07:41 +0100

    feat: add BASE_URL to settings and update URL shortening response format

--- app/core/routes/url.py
+++ app/core/routes/url.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.responses import RedirectResponse
 
 from app.db.database import session_local
-from app.core.services.url_services import generate_short_code, create_short_url
+from app.core.services.url_services import generate_short_code, create_short_url, get_original_url
 from app.core.schemas.url import URLCreate, URLResponse
+
+from app.config.settings import settings
 
 
 router = APIRouter()
@@ -21,5 +24,15 @@ def shorten_url(input:URLCreate, db: Session =  Depends(get_db)):
     url = create_short_url(db, input.original_url)
 
     return {
-        "short_url": url.short_url
+        "short_url": f"{settings.BASE_URL}{url.short_url}"
     }
+
+
+@router.get("/{short_code}")
+def redirect(short_code: str, db: Session = Depends(get_db)):
+    url = get_original_url(db, short_code)
+
+    if not url:
+        raise HTTPException(status_code=404, detail="URL not found")
+    
+    return RedirectResponse(url.original_url)

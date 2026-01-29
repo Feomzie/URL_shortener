bd98ca03471dbc87dc1969ec684a28904537ae8e
Feomzie <joyogboh@gmail.com>
2026-01-29 21:31:32 +0100

    feat: configure Postgres, add secrets to migrate workflow, created vercel config file

--- app/db/database.py
+++ app/db/database.py
@@ -7,7 +7,7 @@ from app.config.settings import settings
 DATABASE_URL = settings.DATABASE_URL
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
